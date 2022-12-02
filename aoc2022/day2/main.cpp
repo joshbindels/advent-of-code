43af23029caf81de445876ba64e6fe1962b53e99
joshbindels <josh.bindels@gmail.com>
2022-12-02 15:43:27 -0500

    day2 part 2

--- aoc2022/day2/main.cpp
+++ aoc2022/day2/main.cpp
@@ -9,6 +9,13 @@ enum HAND
     SCISSORS
 };
 
+enum OUTCOME
+{
+    WIN,
+    LOSE,
+    DRAW
+};
+
 std::map<std::string, HAND> hands = {
     {"A", ROCK},
     {"B", PAPER},
@@ -18,13 +25,30 @@ std::map<std::string, HAND> hands = {
     {"Z", SCISSORS}
 };
 
-bool left_won(HAND left, HAND right)
-{
-    if(left == ROCK && right == SCISSORS)return true;
-    if(left == PAPER && right == ROCK) return true;
-    if(left == SCISSORS && right == PAPER) return true;
-    return false;
-}
+std::map<std::string, OUTCOME> outcomes = {
+    {"X", LOSE},
+    {"Y", DRAW},
+    {"Z", WIN}
+};
+
+std::map<HAND, HAND> handBeats = {
+    {ROCK, SCISSORS},
+    {PAPER, ROCK},
+    {SCISSORS, PAPER}
+};
+
+std::map<HAND, HAND> handLoses = {
+    {ROCK, PAPER},
+    {PAPER, SCISSORS},
+    {SCISSORS, ROCK}
+};
+
+std::map<HAND, int> handValue = {
+    {ROCK, 1},
+    {PAPER, 2},
+    {SCISSORS, 3}
+};
+
 
 int part_a()
 {
@@ -37,18 +61,8 @@ int part_a()
         std::string right = line.substr(line.find(delim) + delim.length(), line.length());
         HAND opp_hand = hands.at(left);
         HAND my_hand = hands.at(right);
-        switch(my_hand)
-        {
-            case ROCK:
-                total_score += 1;
-                break;
-            case PAPER:
-                total_score += 2;
-                break;
-            case SCISSORS:
-                total_score += 3;
-                break;
-        }
+
+        total_score += handValue.at(my_hand);
 
         if(opp_hand == my_hand)
         {
@@ -56,7 +70,7 @@ int part_a()
         }
         else
         {
-            if(left_won(my_hand, opp_hand))
+            if (handBeats.at(my_hand) == opp_hand)
                 total_score += 6;
         }
     }
@@ -64,8 +78,43 @@ int part_a()
     return total_score;
 }
 
+int part_b()
+{
+    std::ifstream instream("input.txt");
+    int total_score = 0;
+    for(std::string line; getline(instream, line);)
+    {
+        std::string delim = " ";
+        std::string left = line.substr(0, line.find(delim));
+        std::string right = line.substr(line.find(delim) + delim.length(), line.length());
+        HAND opp_hand = hands.at(left);
+        OUTCOME outcome = outcomes.at(right);
+        HAND my_hand;
+
+        switch(outcome)
+        {
+            case WIN:
+                my_hand = handLoses.at(opp_hand);
+                total_score += 6;
+                break;
+            case DRAW:
+                my_hand = opp_hand;
+                total_score += 3;
+                break;
+            case LOSE:
+                my_hand = handBeats.at(opp_hand);
+                break;
+        }
+
+        total_score += handValue.at(my_hand);
+
+    }
+    return total_score;
+}
+
 int main()
 {
     std::cout << "Part a: " << part_a() << std::endl;
+    std::cout << "Part b: " << part_b() << std::endl;
     return 0;
 }
